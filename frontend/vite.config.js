3597df9966de29a54a0921835306cfbb604c5409
SupervadoBE <35794721+SupervadoBE@users.noreply.github.com>
2025-07-28 21:35:46 +0300

    fix: added root path to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+// __dirname alternatifi (ESM için)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
+  root: path.resolve(__dirname),
   plugins: [react(), tailwindcss()],
 });
