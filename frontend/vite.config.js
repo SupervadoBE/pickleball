45dc516538ca324e0f6086436f49ebeaf013f685
SupervadoBE <35794721+SupervadoBE@users.noreply.github.com>
2025-07-29 15:55:06 +0300

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// __dirname alternatifi (ESM için)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  root: path.resolve(__dirname),
   plugins: [react(), tailwindcss()],
 });
